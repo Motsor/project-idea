b0a9a20b8e5c4ba0542efe6b9d546163323b8ae1
motsor <rostomle@outlook.com>
2018-09-20 08:57:52 +0300

    Project template

---
+++ src/main/java/fi/project/projectidea/ProjectIdeaApplication.java
@@ -0,0 +1,12 @@
+package fi.project.projectidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIdeaApplication.class, args);
+	}
+}
